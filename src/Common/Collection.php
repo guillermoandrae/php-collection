58089c5a5324f9b5546269855823c6caa1410657
guillermoandrae <guillermoandraefisher@gmail.com>
2019-05-28 15:09:37 -0400

    Added {@inheritDoc} tags, like in the good old days...

--- src/Common/Collection.php
+++ src/Common/Collection.php
@@ -4,33 +4,51 @@ namespace Guillermoandrae\Common;
 
 final class Collection extends AbstractAggregate implements CollectionInterface
 {
+    /**
+     * {@inheritDoc}
+     */
     public static function make(array $items = []): CollectionInterface
     {
         return new static($items);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function all(): array
     {
         return $this->items;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function first()
     {
         return $this->items[0];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function last()
     {
         $array = array_reverse($this->items);
         return $array[0];
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function random()
     {
         $key = array_rand($this->items);
         return $this->get($key);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function sortBy(string $fieldName): CollectionInterface
     {
         $results = $this->items;
@@ -40,16 +58,25 @@ final class Collection extends AbstractAggregate implements CollectionInterface
         return new static($results);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function count(): int
     {
         return count($this->items);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function isEmpty(): bool
     {
         return empty($this->items);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function shuffle(): CollectionInterface
     {
         $items = $this->items;
@@ -57,21 +84,33 @@ final class Collection extends AbstractAggregate implements CollectionInterface
         return new static($items);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function limit(int $offset = 0, int $limit = null): CollectionInterface
     {
         return new static(array_slice($this->items, $offset, $limit));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function filter(callable $callback): CollectionInterface
     {
         return new static(array_filter($this->items, $callback));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function map(callable $callback): CollectionInterface
     {
         return new static(array_map($callback, $this->items));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function toArray(): array
     {
         return array_map(function ($value) {
@@ -79,11 +118,17 @@ final class Collection extends AbstractAggregate implements CollectionInterface
         }, $this->items);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function toJson(): string
     {
         return json_encode($this->toArray());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function __toString(): string
     {
         return $this->toJson();

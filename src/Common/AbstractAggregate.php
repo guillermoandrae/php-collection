92094a9cc726235a3fa9760222474a9813b8fe1c
guillermoandrae <guillermoandraefisher@gmail.com>
2021-12-27 15:45:03 -0500

    PHP 8 updates.

--- src/Common/AbstractAggregate.php
+++ src/Common/AbstractAggregate.php
@@ -3,6 +3,7 @@
 namespace Guillermoandrae\Common;
 
 use ArrayIterator;
+use JetBrains\PhpStorm\Pure;
 
 abstract class AbstractAggregate implements AggregateInterface
 {
@@ -13,7 +14,7 @@ abstract class AbstractAggregate implements AggregateInterface
      *
      * @var array
      */
-    protected $items;
+    protected array $items;
 
     /**
      * Builds the Collection object.
@@ -28,18 +29,18 @@ abstract class AbstractAggregate implements AggregateInterface
     /**
      * {@inheritDoc}
      */
-    final public function has($key): bool
+    final public function has($offset): bool
     {
-        return $this->offsetExists($key);
+        return $this->offsetExists($offset);
     }
 
     /**
      * {@inheritDoc}
      */
-    final public function get($key, $default = null)
+    final public function get(mixed $offset, mixed $default = null): mixed
     {
-        if ($this->offsetExists($key)) {
-            return $this->items[$key];
+        if ($this->offsetExists($offset)) {
+            return $this->items[$offset];
         }
         return $default;
     }
@@ -47,49 +48,49 @@ abstract class AbstractAggregate implements AggregateInterface
      /**
      * {@inheritDoc}
      */
-    final public function set($key, $value)
+    final public function set(mixed $offset, mixed $value): void
     {
-        $this->offsetSet($key, $value);
+        $this->offsetSet($offset, $value);
     }
 
     /**
      * {@inheritDoc}
      */
-    public function remove($key)
+    public function remove($offset): void
     {
-        $this->offsetUnset($key);
+        $this->offsetUnset($offset);
     }
 
     /**
      * {@inheritDoc}
      */
-    final public function offsetExists($key)
+    final public function offsetExists($offset): bool
     {
-        return array_key_exists($key, $this->items);
+        return array_key_exists($offset, $this->items);
     }
 
     /**
      * {@inheritDoc}
      */
-    final public function offsetGet($key)
+    final public function offsetGet($offset): mixed
     {
-        return $this->items[$key];
+        return $this->items[$offset];
     }
 
     /**
      * {@inheritDoc}
      */
-    final public function offsetSet($key, $value)
+    final public function offsetSet($offset, $value): void
     {
-        $this->items[$key] = $value;
+        $this->items[$offset] = $value;
     }
 
     /**
      * {@inheritDoc}
      */
-    final public function offsetUnset($key)
+    final public function offsetUnset($offset): void
     {
-        unset($this->items[$key]);
+        unset($this->items[$offset]);
     }
 
     /**
--- src/Common/AggregateInterface.php
+++ src/Common/AggregateInterface.php
@@ -5,38 +5,39 @@ namespace Guillermoandrae\Common;
 interface AggregateInterface extends \ArrayAccess, \IteratorAggregate
 {
     /**
-     * Determines whether or not the item associated with the provided key
+     * Determines whether or not the item associated with the provided offset
      * exists.
      *
-     * @param mixed $key  The key associated with the desired item.
+     * @param mixed $offset  The offset associated with the desired item.
      * @return bool
      */
-    public function has($key): bool;
+    public function has(mixed $offset): bool;
 
     /**
-     * Returns the item associated with the provided key. If no item exists,
+     * Returns the item associated with the provided offset. If no item exists,
      * returns the provided default value.
      *
-     * @param mixed $key  The key associated with the desired item.
+     * @param mixed $offset  The offset associated with the desired item.
      * @param mixed|null $default  The default value.
      * @return mixed
      */
-    public function get($key, $default = null);
+    public function get(mixed $offset, mixed $default = null): mixed;
 
     /**
-     * Registers an item using the provided key and value.
+     * Registers an item using the provided offset and value.
      *
-     * @param mixed $key  The desired key.
+     * @param mixed $offset  The desired offset.
      * @param mixed $value  The desired value.
-     * @return mixed
+     * @return void
      */
-    public function set($key, $value);
+    public function set(mixed $offset, mixed $value): void;
 
     /**
-     * Removes the item associated with the provided key and returns the new
+     * Removes the item associated with the provided offset and returns the new
      * collection.
      *
-     * @param mixed $key  The key associated with the desired item.
+     * @param mixed $offset  The offset associated with the desired item.
+     * @return void
      */
-    public function remove($key);
+    public function remove(mixed $offset): void;
 }
